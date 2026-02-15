cb9f33025da2447ccb1626909cb71b69ab4be8f8
tzuhuanl <tzuhuanl@gmail.com>
2026-02-15 23:00:38 +0100

    Spring security update (with default username and password)

---
+++ simpleWebApp/src/main/java/com/simpleWebApp/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.simpleWebApp.repository;
+
+import com.simpleWebApp.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+
+}
--- simpleWebApp/src/main/java/com/simpleWebApp/service/ProductService.java
+++ simpleWebApp/src/main/java/com/simpleWebApp/service/ProductService.java
@@ -3,17 +3,25 @@ package com.simpleWebApp.service;
 
 import com.simpleWebApp.exception.ResourceNotFoundException;
 import com.simpleWebApp.model.Product;
+import com.simpleWebApp.model.Users;
 import com.simpleWebApp.repository.ProductRepo;
+import com.simpleWebApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class ProductService {
+public class ProductService{
 
     @Autowired
     ProductRepo repo;
+
+    @Autowired
+    UserRepo userRepo;
 
     public List<Product> getProductList(){
 
@@ -25,7 +33,17 @@ public class ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("Cannot find No. " + id + " product."));
     }
 
-    public void addProduct(Product product){
+    public void addProduct(Product product, String username){
+
+        Users user = userRepo.findByUsername(username);
+        if(user == null){
+            throw new ResourceNotFoundException("User not found");
+        }
+
+        product.setUser(user);
+
+        repo.save(product);
+
         repo.save(product);
     }
 
